6ad446003dab83ed2874c1da8dd53976b0ee78de
Robert Stepp <robert@robertstepp.ninja>
2018-04-01 19:51:33 -0700

    Started Programming

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,22 @@
+import javax.swing.JOptionPane;
 
+public class Main {
+	public static processes procs = new processes();
+
+	public static void main(String[] args) {
+		int columns = Integer.parseInt(
+				JOptionPane.showInputDialog("How many digits to check to?"));
+		if (columns < 1) {
+			JOptionPane.showMessageDialog(null,
+					"No digits requested. Please restart.");
+		} else if (columns > 10) {
+			JOptionPane.showMessageDialog(null,
+					"Unsure of result at this time. Please try with a number lower then 10.");
+		} else {
+			procs.createDict(columns);
+			// System.out.println(procs.retriveDict());
+		}
+		System.out.println(procs.retrivePrevious());
+
+	}
+}
--- src/processes.java
+++ src/processes.java
@@ -1,4 +1,46 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class processes {
+	public static Dictionary<Integer, Integer> numbers = new Hashtable<Integer, Integer>();
+	private int columnCount = 0;
+	private ArrayList<BigInteger> previousIterations = new ArrayList<>();
+
+	public void createDict(int columns) {
+		columnCount = columns;
+		for (int i = columns - 1; i >= 0; i--) {
+			numbers.put(i, 1);
+		}
+		createNumber();
+
+	}
+
+	public Dictionary<Integer, Integer> retriveDict() {
+		return numbers;
+
+	}
+
+	public ArrayList<BigInteger> retrivePrevious() {
+		return previousIterations;
+	}
+
+	// public void countNumbers() {
+	// ArrayList<Integer> countNumber;
+	// for (int i = 0; i < columnCount; i++) {
+	//
+	// }
+	//
+	// }
+	public void createNumber() {
+		String number = "";
+		for (int it = columnCount - 1; it >= 0; it--) {
+			number = number + numbers.get(it) + it;
+
+		}
+		BigInteger numberParsed = new BigInteger(number);
+		previousIterations.add(numberParsed);
+	}
 
 }
